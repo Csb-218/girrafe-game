607398513da9c26c501ac353aa2bea41641efa3b
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-05-30 03:00:07 +0530

    feat : added icons

---
+++ src/components/icons/IconEscape.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg width="45" height="49" viewBox="0 0 45 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_782_12646)">
+<circle cx="22.5" cy="22.5" r="22" fill="white" stroke="#E4E4E4"/>
+<path d="M16 22H28" stroke="#000D26" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M19.6 25.6L16 22L19.6 18.4" stroke="#000D26" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<filter id="filter0_d_782_12646" x="0" y="0" width="45" height="49" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0.894118 0 0 0 0 0.894118 0 0 0 0 0.894118 0 0 0 1 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_782_12646"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_782_12646" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+</template>
