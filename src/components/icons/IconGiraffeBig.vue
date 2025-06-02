51fec916f03b1c27d6a51abde3e974bde5ff2463
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-06-03 00:10:53 +0530

    Create IconGiraffeBig.vue

---
+++ src/components/icons/IconGiraffeBig.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import giraffe_long from "@/assets/ripple-game/game-characters/Giraffe/giraffe_long.svg"
+import giraffe_sad from "@/assets/ripple-game/game-characters/Giraffe/GiraffeSad.svg"
+import giraffe_idle from "@/assets/ripple-game/game-characters/Giraffe/GiraffeIdle.svg"
+import giraffe_happy from "@/assets/ripple-game/game-characters/Giraffe/GiraffeHappy.svg"
+import giraffe_confused from "@/assets/ripple-game/game-characters/Giraffe/GiraffeConfused.svg"
+</script>
+
+<template>
+
+  <div class="relative h-[255px] w-[82px] overflow-hidden ">
+    <img
+      :src="giraffe_idle"
+      class=" object-contain z-20 relative"
+      alt="Giraffe Idle"
+    />
+    <img
+      :src="giraffe_long"
+      class="w-[34px] absolute left-[30%] -bottom-5 object-contain z-10"
+      alt="Giraffe"
+    />
+  </div>
+
+</template>
+<style scoped>
+</style>
