2f7660650cbece44215669b3415314abc25b6377
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-06-03 00:11:58 +0530

    feat : added function checkIfSorted

---
+++ src/utils/helpers.ts
@@ -0,0 +1,8 @@
+export function checkIfSorted(arr: number[]): boolean { // Ascending order check
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
